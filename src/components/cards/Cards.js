835040014055643f8b4d92e583204556f85fff07
Nizamettin KAYA <kaya_nk@hotmail.com>
2021-08-10 22:37:09 +0300

    second changes

---
+++ src/components/cards/Cards.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import "./Cards.css";
+@import url('https://fonts.googleapis.com/css2?family=WindSong:wght@500&display=swap');
+
+const Cards = () => {
+    return (
+        <div className="img-container">
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj sdjfsdl skdjfklsd skljdfkdsfj sakljdkls jdkfjkd sksldjf kass sjfpd djkf sd skjd s ps jopskws pwjfpsoj o opww jops jopw jwop jo jo jospjopej poğprnc0djf0  jpwj0sd j0wj   s jpodsfjoğ0w j dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+            </div>
+            <div className="img-container">
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p className="p-1">lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+            </div>
+            <div className="img-container">
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            <div className="c-1">
+                <h3>Turkey</h3>
+                <img src="https://images.unsplash.com/photo-1510253687831-0f982d7862fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"/>
+                <div className="text">
+                <p>lorem10dsfhsdfj dfdf dfdsf dsfdsf  djdsfjıoe sıdfıjosjf asafjosdjf dsıfjos</p>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+export default Cards;
+
